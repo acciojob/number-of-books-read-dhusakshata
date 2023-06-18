811a2dbc43723e8aae8b0a5b4e4c6c7ae3f3bc5b
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-06-18 13:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,25 +26,6 @@ const numberOfBooksRead = (library) => {
   return count;
 };
 
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-alert(numberOfBooksRead(library));
 
 // Do not change the code below
 
